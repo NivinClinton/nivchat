bd1e2a145bb6aba46618e2bf294dbe319c3adeef
NivinClinton <nivinclint@gmail.com>
2022-09-08 18:16:27 +0530

    firebase configuration deleted

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGeF-2N1UecaVE5v8FCs6NZtuaqwgH36k",
-  authDomain: "nivchat-786d3.firebaseapp.com",
-  projectId: "nivchat-786d3",
-  storageBucket: "nivchat-786d3.appspot.com",
-  messagingSenderId: "753587028252",
-  appId: "1:753587028252:web:d6f468cee6e9f1b3ad3ad9"
+  //
 };
 
 
